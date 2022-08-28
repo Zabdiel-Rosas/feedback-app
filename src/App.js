b9263f8222ac91e7f7737e14fc762f8f0d270602
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2022-08-28 18:54:29 -0500

    FeedbackItem component is created and implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import Header from "./components/Header";
+import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <div className='container'>
-        <h1>My App</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <div className='container'>
+              <FeedbackItem />
+            </div>
+        </>
+    );
 }
 
 export default App;
