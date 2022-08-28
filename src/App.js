c7439a8b64c66927acbb9a05d73d96010bbfb9d2
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2022-08-28 16:13:30 -0500

    creating First Component and Props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post';
-
-  const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'},
-  ]
-
-  const loading = false;
-  const showComments = true;
-
-  if (loading) return <h1>Loading...</h1>
-
   return (
-    <div className='container'>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments && (
-        <div className='comments'>
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>{comment.text}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <>
+      <Header text='This is the Header'/>
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
