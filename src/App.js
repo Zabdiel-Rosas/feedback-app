b9aa3c6fb8b913c8e2bbbc4dea1dbda48ddfeb00
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2022-09-10 21:56:03 -0500

    FeedbackForm component is created and implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackData from './data/FeedbackData';
@@ -21,6 +22,7 @@ function App() {
         <>
             <Header />
             <div className='container'>
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList
                     list={feedback}
